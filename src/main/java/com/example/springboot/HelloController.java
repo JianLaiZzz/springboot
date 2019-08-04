87bc078e5705d508e53502887d297d856f4e6d35
zhw <1914624121@qq.com>
2019-08-04 23:04:22 +0800

    ideal 整合github测试
    第一个springboot项目

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController public class HelloController
+{
+	@Value("${name}") private String name;
+
+	@Value("${age}") private int age;
+
+	@RequestMapping("/hello") public String hello()
+	{
+		return name+age;
+	}
+}
